8d080ad02531a375b172a5d128d8f915d9afde43
knknkn1162 <knknkn1162@gmail.com>
2022-02-04 10:06:39 +0900

    improve abc085_c.ps1

--- src/abc085_c.ps1
+++ src/abc085_c.ps1
@@ -1,10 +1,11 @@
 [int]$n, [int]$y = ([string]$input).split(" ")
 
 # dummy
-$arr = 0..$n |`
+$arr = @(,@(-1,-1,-1))
+$arr += @(0..$n |`
     %{$x=$_; 0..($n-$x) |`
         ?{$x * 10000 + $_ * 5000 + ($n - $x - $_) * 1000 -eq $y} |`
-        %{@($x, $_, [int]($n - $x - $_))}
+        %{,@($x, $_, [int]($n - $x - $_))}
     }
-$ret = if($arr.length -eq 0) {@(-1,-1,-1)} else {$arr[0..2]}
-$ret -join " "
+)
+$arr[-1] -join " "
